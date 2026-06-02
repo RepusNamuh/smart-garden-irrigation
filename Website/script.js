0282d432cd2b89a1cec9c29c9d42f9ef314211cc
Repus <congle310610@gmail.com>
2026-06-02 19:27:40 +1000

    Temporary push to pull from raspeberry pi for development and testing

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+dynamicContent = document.getElementById("devices_list");
+
+/*Now, need to be able to load device information
+Need to set the container size between the boundary of smallest 
+to largest possible for the device list.
+Once the container size reaches max, enable scrolling.
+*/
